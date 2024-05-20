328830646d2623802afe8b60b4714c15c527e2f9
AlejandroTorres11 <atorresdesarrollo@gmail.com>
2024-05-20 13:21:40 +0200

    v.0.03

--- raycasting.py
+++ raycasting.py
@@ -3,11 +3,35 @@ import math
 from settings import *
 
 class RayCasting:
-    def __init__(self):
+    def __init__(self,game):
         self.game= game
     
-    def rayCast(self):
-        pass
-    
+    def rayCast(self,player):
+        ox,oy= self.game.player.pos
+        xMap,yMap= self.game.player.mapPos
+        rayAngle= self.game.player.angle - HALF_FOV +0.0001
+        for ray in range(NUM_RAYS):
+            sinA=math.sin(rayAngle)
+            cosA=math.cos(rayAngle)
+
+            #lineas verticales de la casilla
+            if cosA>0: #significa que miramos a derecha
+                xVert,dx=(xMap+1,1)
+            else: #si miramos a izquierda
+                xVert,dx=xMap-1e-6,-1 
+            depthVert=(xVert -ox)/cosA #trigonometria
+            yVert=oy + sinA* depthVert
+
+            for i in range(MAX_DEPTH):
+                tileVert= int(xVert),int(yVert)
+                if tileVert in self.game.map.worldMap:
+                    break
+                xVert+=dx
+                yVert+=dy
+                depthVert+= deltaDepth
+            deltaDepth= dx/cosA  #deltaDepth es la distancia al siguiente cuadrante
+            dy= deltaDepth * sinA
+            rayAngle+=DELTA_ANGLE
+            pg.draw.line(self.game.screen,'yellow',(self.x *100, self.y *100 ), (ox+MAX_DEPTH*sinA,oy +MAX_DEPTH*cosA),2)    
     def update(self):
         self.rayCast()
--- settings.py
+++ settings.py
@@ -1,10 +1,23 @@
+import math
+
 #opciones generales
+
+#pantalla
 RES = WIDTH, HEIGHT = 1600,900
 FPS=60
+
+#jugador
 PLAYER_POS=1.5 , 5 #miniMap
 PLAYER_ANGLE= 0
 PLAYER_SPEED= 0.04
 PLAYER_ROT_SPEED = 0.05
+#ray cast
+FOV = math.pi/3 
+HALF_FOV= FOV/2
+NUM_RAYS=120
+MAX_DEPTH= 800  #longitud maxima de un rayo
+DELTA_ANGLE= FOV/NUM_RAYS #diferencia de grados entre rayos
+#proyectil
 PROYECTILE_POS=PLAYER_POS #miniMap
 PROYECTILE_ANGLE= PLAYER_ANGLE
 PROYECTILE_SPEED= 0.04
